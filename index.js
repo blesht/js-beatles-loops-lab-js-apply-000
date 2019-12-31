cfd9bdd2b6ba7cea1fc1af6cae6ddfd46587dd28
upbeat-queue-5483 <bryan.lesht@colorado.edu>
2019-12-31 03:54:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+
+
 function theBeatlesPlay(musicians, instruments) {
 
 for (let i =0; i<(musicians.length);i++) {
