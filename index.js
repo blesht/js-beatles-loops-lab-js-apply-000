d50ffe884285eaf4d6ac3448313f3bc9129c1305
upbeat-queue-5483 <bryan.lesht@colorado.edu>
2019-12-31 03:37:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // add solution here
-function theBeatlesPlay(musicians, instrucments) {
-let musicians = []
-let instruments = []
-for (let i =0; i<(musicians.length +1);i++) {
+function theBeatlesPlay(musicians, instruments) {
+
+for (let i =0; i<(musicians.length);i++) {
   console.log(`${musicians[i]} plays ${instruments[i]}`)
   }
 }
