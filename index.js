40529e5170a169199c1a2c16cd9360d3630d4ef6
upbeat-queue-5483 <bryan.lesht@colorado.edu>
2019-12-31 04:01:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts(facts) {
   let arraycounter = 0
   let factsEx = []
   while (facts.length > arraycounter) {
-  factsEx.push(`${facts[arraycounter]}!!!`)
+  factsEx.push(` ${facts[arraycounter]}!!!`)
   arraycounter += 1
   console.log(factsEx)
   
