a028f3365f1bd2502035a8e670fe7f35c4c4a4c2
upbeat-queue-5483 <bryan.lesht@colorado.edu>
2019-12-31 03:30:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
 function theBeatlesPlay(musicians, instrucments) {}
 let musicians = []
-for (let i =0; i<(musicians.length +1));i++)
-  return `${musicians[i]} plays ${instruments}[i]
+for (let i =0; i<(musicians.length +1);i++)
+  return `${musicians[i]} plays ${instruments[i]}`
